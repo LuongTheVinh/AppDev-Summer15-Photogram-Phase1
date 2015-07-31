d05c8aaf35485cf7422b3eb7afe143264adcd579
MBA Learns to Code <MBALearnsToCode@UChicago.edu>
2015-07-30 22:20:53 -0500

    submit homework

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,7 +1,26 @@
 class PhotosController < ApplicationController
   def index
     @list_of_photos = Photo.all
+    render('index.html.erb')
   end
 
+  def show
+    photo_data = Photo.find(params['id'])
+    @photo_source = photo_data.source
+    @photo_caption = photo_data.caption
+
+    render('show.html.erb')
+  end
+
+  def destroy
+    Photo.find(params['id']).destroy
+    @list_of_photos = Photo.all
+
+    render('index.html.erb')
+  end
+
+  def new_form
+    render('new_form.html.erb')
+  end
 
 end
